48aaa0c3e124f5078bbc39b9190d924c6b59da9e
Celeste Bareiro <cnyx7@proton.me>
2023-02-17 21:20:23 +0000

    kept working on CSS, HTML, and started with JS

--- src/app.js
+++ src/app.js
@@ -6,8 +6,7 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 // CARDS HTML FOR HEARTS, SPADES, CLUBS AND DIAMONDS (VARIABLES?)
+let cardSuits = ["$hearts;", "$spades;", "$clubs;", "$clubs;"];
+let cardNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, J, Q, K];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+window.onload = function() {};
