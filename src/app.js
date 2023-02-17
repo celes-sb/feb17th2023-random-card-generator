5b2fe95619f6884cd6b9a0137937edd91d2de04e
Celeste Bareiro <cnyx7@proton.me>
2023-02-17 20:06:35 +0000

    .html and .css

--- src/app.js
+++ src/app.js
@@ -5,6 +5,8 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+// CARDS HTML FOR HEARTS, SPADES, CLUBS AND DIAMONDS (VARIABLES?)
+
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
