f59707923b4e33b344c2cff55a7a7f6083f2b1c9
Celeste Bareiro <cnyx7@proton.me>
2023-02-17 23:09:24 +0000

    last commit before final corrections

--- src/app.js
+++ src/app.js
@@ -5,8 +5,35 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-// CARDS HTML FOR HEARTS, SPADES, CLUBS AND DIAMONDS (VARIABLES?)
-let cardSuits = ["$hearts;", "$spades;", "$clubs;", "$clubs;"];
-let cardNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, J, Q, K];
+const icons = ["♦", "♥", "♠", "♣"];
+const suits = ["diamondSymbol", "heartSymbol", "spadeSymbol", "clubSymbol"];
+const values = [
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "K",
+  "Q",
+  "J",
+  "A"
+];
 
-window.onload = function() {};
+const randomSuitIndex = Math.floor(Math.random() * suits.length);
+const randomValueIndex = Math.floor(Math.random() * values.length);
+const randomIconsIndex = Math.floor(Math.random() * icons.length);
+
+const cardHTML = `
+<div id="card-symbol-top" class="cardSymbol ${suits[randomSuitIndex]}">${icons[randomIconsIndex]}</div>
+      <div class="card-number">
+        <h2>${values[randomValueIndex]}</h2>
+      </div>
+      <div id="card-symbol-bottom" class="cardSymbol ${suits[randomSuitIndex]}">${icons[randomIconsIndex]}</div>
+`;
+
+const cardContainer = document.querySelector("#card");
+cardContainer.innerHTML = cardHTML;
