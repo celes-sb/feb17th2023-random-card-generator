ad6e5ef72bcfbe0e5129d3b7b4766ac5cd094e81
Celeste Bareiro <cnyx7@proton.me>
2023-02-20 15:50:59 +0000

    final changes done: corrected JS and CSS

--- src/app.js
+++ src/app.js
@@ -4,36 +4,55 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+import { applyStyles } from "@popperjs/core";
 
-const icons = ["♦", "♥", "♠", "♣"];
-const suits = ["diamondSymbol", "heartSymbol", "spadeSymbol", "clubSymbol"];
-const values = [
-  "2",
-  "3",
-  "4",
-  "5",
-  "6",
-  "7",
-  "8",
-  "9",
-  "10",
-  "K",
-  "Q",
-  "J",
-  "A"
-];
+window.onload = function() {
+  //write your code here
 
-const randomSuitIndex = Math.floor(Math.random() * suits.length);
-const randomValueIndex = Math.floor(Math.random() * values.length);
-const randomIconsIndex = Math.floor(Math.random() * icons.length);
+  let switchCardButton = document.querySelector("#button");
+  switchCardButton.addEventListener("click", function() {
+    let icons = ["♦", "♥", "♠", "♣"];
+    let values = [
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "K",
+      "Q",
+      "J",
+      "A"
+    ];
 
-const cardHTML = `
-<div id="card-symbol-top" class="cardSymbol ${suits[randomSuitIndex]}">${icons[randomIconsIndex]}</div>
-      <div class="card-number">
-        <h2>${values[randomValueIndex]}</h2>
-      </div>
-      <div id="card-symbol-bottom" class="cardSymbol ${suits[randomSuitIndex]}">${icons[randomIconsIndex]}</div>
+    let randomValueIndex = Math.floor(Math.random() * values.length);
+    let randomIconsIndex = Math.floor(Math.random() * icons.length);
+    let randomSuits = Math.floor(Math.random() * icons.length);
+
+    function colorSuits(icon) {
+      if (icon === "♠" || icon === "♣") {
+        return "black";
+      } else {
+        return "red";
+      }
+    }
+
+    let cardHTML = `
+  <div id="card-symbol-top" class="cardSymbol ${colorSuits(
+    icons[randomIconsIndex]
+  )}">${icons[randomIconsIndex]}</div>
+  <div class="card-number">
+    <h2>${values[randomValueIndex]}</h2>
+  </div>
+  <div id="card-symbol-bottom" class="cardSymbol ${colorSuits(
+    icons[randomIconsIndex]
+  )}">${icons[randomIconsIndex]}</div>
 `;
 
-const cardContainer = document.querySelector("#card");
-cardContainer.innerHTML = cardHTML;
+    let cardContainer = document.querySelector("#card");
+    cardContainer.innerHTML = cardHTML;
+  });
+};
